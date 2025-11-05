f7cb508c6e3e10aa9cea0c54a8c302f0a896e78f
Jupower38300 <julienlegrand038@gmail.com>
2025-11-05 16:56:18 +0100

    Fin TP2 qui fonctionne :D

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import http from "http";
 import cors from "cors";
 import { ChatController } from "./api/chat.controler.js";
 import { WhoAmControler } from "./api/whoami.controler.js";
-import cors from "cors";
 
 
-export const clients = ["172.20.10.4","172.20.10.3"]; // <--- Liste d'IP clients
+export const clients = []; // <--- Liste d'IP clients
 
 const PORT = 3000;
 const app = express();
 app.use(cors());
+app.use(express.json());
 const server = http.createServer(app);
 
 // Enregistrement d'un client (Client A)

ebf3abc38785bb2d777306e38280e9a47fb8adfe
Raphael DANJARD-MAZA <raphael.danjard@hotmail.com>
2025-11-05 14:31:41 +0100

    OK

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 import express from "express";
 import http from "http";
+import cors from "cors"; // ✅
+
 import { ChatSocket } from "./chat/chat.socket.js";
 import { ChatControler } from "./api/chat.controler.js";
 import { WhoAmControler } from "./api/whoami.controler.js";
 
 const PORT = 3000;
 const app = express();
+app.use(cors()); // ✅ autorise l'accès du front
+app.use(express.json());
+
 const server = http.createServer(app);
 
 // Websocket
 ChatSocket.INSTANCE.setup(server);
 
-// Middlewares
-app.use(express.static("../front/dist"));
-app.use(express.json());
-
 // API
 app.get("/api/whoami", WhoAmControler.get);
 app.post("/api/chat", ChatControler.post);
 
-// Écoute serveur
+// Lancement
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
